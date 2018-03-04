8d8d3cf9c40f295aeb45b88ad87603921d0a1592
Christopher Demeke <christopherdemeke@dmk.attlocal.net>
2018-03-04 17:25:58 -0600

    Second Draft with new features for adding 10 more gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,27 +4,41 @@ $(function () {
     console.log("hello chris");
 
     var topic = "NBA Player";
-    var topics = ["Micheal Jordan", "LeBron James", "Kobe Byrant", "Shaquille O'neal", "Stephen Curry", "Charles Barkley", "Kevin Durant", "Tim Duncan", "Carmelo Anthony", "James Harden", "Chris Paul", "Hakeem Olajuwon"];
+    var topics = ["Micheal Jordan", "LeBron James", "Kobe Byrant", "Shaquille O'neal", "Stephen Curry", "Charles Barkley", "Kevin Durant", "Carmelo Anthony", "James Harden", "Chris Paul"];
+    var addTenMore = false;
+    var value = null;
+    var selectedButton = null;
+    var limit = 10
 
     function displayGif() {
         $(".topicGifs").empty();
-        var value = $(this).attr("data-name").split(' ').join('+');
-        var limit = 10
+        if (addTenMore == true) {
+            value = selectedButton;
+            limit = limit + 10;
+        } else {
+            var value = $(this).attr("data-name").split(' ').join('+');
+            selectedButton = value;
+        }
         var APIKey = "dVeZE1jTUX9IzYurR1xbjnVHWuWBSLH1";
         var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + value + '&api_key=' + APIKey + '&limit=' + limit;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // console.log(response);
+            console.log(response);
             for (var i = 0; i < limit; i++) {
                 var result = response.data
                 var rating = result[i].rating;
-                var pOne = $("<p>")
+                var title = result[i].title;
+                var type = result[i].type;
+                var pOne = $("<p>");
+                var pTwo = $("<p>");
+                var pThree = $("<p>");
                 var gifDiv = $("<div>");
                 var gifImage = $("<img>");
-
-                pOne.text("Rating : " + rating);
+                pOne.text("Title : " + title);
+                pTwo.text("Rating : " + rating);
+                pThree.text("Type : " + type);
                 gifDiv.addClass("gif");
                 gifImage.addClass("gifImage");
                 gifImage.attr("src", result[i].images.fixed_height_small_still.url);
@@ -34,6 +48,8 @@ $(function () {
                 gifImage.attr("data-state", "still");
                 gifDiv.append(gifImage);
                 gifDiv.append(pOne);
+                gifDiv.append(pTwo);
+                gifDiv.append(pThree);
                 $(".topicGifs").append(gifDiv);
             }
             $(".gifImage").on("click", function () {
@@ -47,6 +63,7 @@ $(function () {
                 }
             })
         });
+        addTenMore = false;
     }
 
 
@@ -68,6 +85,16 @@ $(function () {
         topics.push(newTopic);
         createButtons();
     });
+
+    $(".addMore").on("click", function () {
+        if (selectedButton === null) {
+            console.log(selectedButton);
+        } else {
+            addTenMore = true;
+            displayGif();
+        }
+    });
+
 
     $(".navbarTitle").text("Giftastic: " + topic + " Edition");
     $("#itemInputLabel").text("Add " + topic);
